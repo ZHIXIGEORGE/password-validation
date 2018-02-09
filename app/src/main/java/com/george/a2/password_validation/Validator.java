8d00094bf6a452742cdea5ab5f5f36f05b9c0c1f
ZHIXIGEORGE <35536765+ZHIXIGEORGE@users.noreply.github.com>
2018-02-09 15:47:15 -0500

    stage2

--- app/src/main/java/com/george/a2/password_validation/Validator.java
+++ app/src/main/java/com/george/a2/password_validation/Validator.java
@@ -3,16 +3,22 @@ package com.george.a2.password_validation;
 /**
  * Created by GEORGE on 2018-02-07.
  */
+import java.util.regex.Pattern
 
 public class Validator {
 
     public int validate(String s){
 
         int ret=0;
-        //checking 2 rules
         if(!(s.toLowerCase().equals("password")))
             ret++;
         if(!(s.length()<8))
+            ret++;
+        if(Pattern.matches(".*\\d.*", s))//check does it contain a number or not
+            ret++;
+        if(Pattern.matches(".*\\W.*", s))//check does it contain a special character or not
+            ret++;
+        if(!s.isEmpty())
             ret++;
 
         return ret;
--- app/src/test/java/com/george/a2/password_validation/ExampleUnitTest.java
+++ app/src/test/java/com/george/a2/password_validation/ExampleUnitTest.java
@@ -13,11 +13,33 @@ public class ExampleUnitTest {
     @Test
     public void checkNotPassword() throws Exception {
         Validator validator = new Validator();
-        assertEquals(1  , validator.validate("pasward"));
+        assertEquals(2  , validator.validate("pasward"));
     }
     @Test
     public void checkLength() throws Exception {
         Validator validator = new Validator();
-        assertEquals(2  , validator.validate("passward"));
+        assertEquals(3  , validator.validate("passward"));
     }
+    @Test
+    public void checkPassword() throws Exception {
+        Validator validator = new Validator();
+        assertEquals(2  , validator.validate("password"));
+    }
+    @Test
+    public void checknum() throws Exception {
+        Validator validator = new Validator();
+        assertEquals(4  , validator.validate("password1"));
+    }
+    @Test
+    public void checkEmpty() throws Exception {
+        Validator validator = new Validator();
+        assertEquals(4  , validator.validate("passward1"));
+    }
+    @Test
+    public void checkSpecial() throws Exception {
+        Validator validator = new Validator();
+        assertEquals(5  , validator.validate("passward1*"));
+    }
+
+
 }
